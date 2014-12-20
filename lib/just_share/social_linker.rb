5c00c61c1481bc291672b50175c7f4223fd5f018
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-20 18:27:46 -0200

    - attrs refactored (removed all unnecessary); - facade & polymorphic methods created: get_post_link + setup_attrs.

--- lib/just_share/social_linker.rb
+++ lib/just_share/social_linker.rb
@@ -1,27 +1,32 @@
 # ParentClass which have the attrs to create the links
 class JustShare::SocialLinker
   # Attrs accessor for it classes
-  attr_accessor :title # Title on the post
   attr_accessor :message # Base Message (like from the Ad.social_message)
   attr_accessor :link # The redirect link on the post
   attr_accessor :via # The app which post like: PageRenter
-  attr_accessor :image_url # The image to be shown
 
   # Attrs filled on children
   attr_accessor :domain # ex: facebook.com twitter.com ...
   attr_accessor :path # ex: facebook.com twitter.com ...
+  attr_accessor :params # ex: facebook.com twitter.com ...
 
   # It constructor must be reused by it children
   def initialize(params={})
     # SetUp the attrs
-    self.via=params[:via]
-    self.link=params[:link]
-    self.title=params[:title]
-    self.message=params[:message]
-    self.image_url=params[:image_url]
+    self.via=params[:via] || self.via=JustShare.via
+    self.link=params[:link] || self.link=JustShare.link
+    self.message=params[:message] || self.message=JustShare.message
+  end
+
+  # FacadeMethod
+  def get_post_link
+    # SetUp attrs
+    setup_attrs
+    # Create & return it URL created
+    "#{self.domain}/#{self.path}?#{params}"
   end
 
   # Method to be implemented on it children!
-  def get_post_link
+  def setup_attrs
   end
 end
