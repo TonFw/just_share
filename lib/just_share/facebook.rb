4554a93cc1f6b21112c02df2b629311f9b606eb0
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-20 19:03:24 -0200

    facebook URL generator working pretty well

--- lib/just_share/facebook.rb
+++ lib/just_share/facebook.rb
@@ -1,2 +1,13 @@
-class JustShare::Facebook
+class JustShare::Facebook < JustShare::SocialLinker
+  def setup_attrs
+    # Base URL
+    self.domain='https://www.facebook.com'
+    self.path='sharer/sharer.php'
+
+    # Params (only the link is working, the others are deprecated for the sharer.php & won't work)
+    url_param = "u=#{self.link}"
+
+    # build the params
+    self.params = "#{url_param}"
+  end
 end
